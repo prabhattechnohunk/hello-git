1150d8768ec0bab6fe4ec5ff1fcbb877fc7199d9
durga.maharjan.dev <durga.maharjan.dev@gmail.com>
2024-06-13 06:20:28 -0400

    bug-fix

---
+++ src/com/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Day 1 work done.");
+		System.out.println("Day 2 work done.");
+		
+	}
+
+}
